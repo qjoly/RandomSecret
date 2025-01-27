dfba620d6767f066e2c044376e4eac5f67f291c9
Quentin Joly <quentinj@une-pause-cafe.fr>
2025-01-27 18:32:26 +0100

    Create Repo

---
+++ README.md
@@ -0,0 +1,60 @@
+# RandomSecrets Operator
+
+:warning: **This operator is not intended for production use.**
+
+This operator is a project built for educational purposes. It is a simple operator that modifies a secret to add a random value to it. It is built to demonstrate how to build an operator without using any frameworks.
+
+**Features:**
+- Adds a random value to a secret.
+- Takes configuration from annotations (if the secret has to be updated, which key to update, if the random value should contain special characters).
+- Use a CRD to define the configuration of a secret (which will be created by the operator).
+- Use a MutatingWebhook to intercept the creation of the secret and add the random value to it.
+
+## Installation
+
+Install the operator by deploying the Helm chart:
+
+```bash
+helm install randomsecrets-operator ./helm
+```
+
+## Usage
+
+### With a CRD
+
+Create a `RandomSecret` resource to define the configuration of the secret:
+
+```yaml
+apiVersion: secret.a-cup-of.coffee/v1
+kind: RandomSecret
+metadata:
+  name: random-secret
+  namespace: default
+spec:
+  key: password
+  secretName: secret-name
+  length: 32
+  static: 
+    username: admin
+    email: "quentin@a-cup-of.coffee"
+```
+
+### With annotations
+
+Create a secret with the following annotations:
+
+```yaml
+apiVersion: v1
+kind: Secret
+metadata:
+  annotations:
+    secret.a-cup-of.coffee/enable: "true"
+    secret.a-cup-of.coffee/key: password
+  name: non-empty-secret
+  namespace: coder
+type: Opaque
+```
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
---
+++ pkg/secrets/secrets.go
@@ -0,0 +1,149 @@
+package secrets
+
+import (
+	"context"
+	"encoding/base64"
+	"fmt"
+	"strconv"
+	"time"
+
+	"github.com/qjoly/randomsecret/pkg/types"
+	"golang.org/x/exp/rand"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/klog"
+)
+
+// getRandomSecretKey returns the key used to store the random secret
+// Check if the annotations map contains the key annotation
+// If it does, return the key
+func getRandomSecretKey(secret v1.Secret) string {
+	key, ok := secret.Annotations[types.OperatorSecretKeyAnnotation]
+	if !ok {
+		klog.Info("Secret does not have a key annotation, using default key")
+		return types.OperatorDefaultSecretKey
+	}
+	klog.Info("Secret has a key annotation, using key: ", key)
+	return key
+}
+
+func IsSecretManaged(secret v1.Secret) bool {
+	value, ok := secret.Annotations[types.OperatorEnabledAnnotation]
+	if !ok {
+		return false
+	}
+
+	if value != "true" {
+		return false
+	}
+
+	return ok
+}
+
+func isAlreadyHandled(secret v1.Secret) bool {
+	_, ok := secret.Data[getRandomSecretKey(secret)]
+	return ok
+}
+
+func HandleSecrets(clientset *kubernetes.Clientset, secret v1.Secret) {
+	klog.Info(fmt.Sprintf("Handling secret: %s with key %s", secret.Name, getRandomSecretKey(secret)))
+
+	if isAlreadyHandled(secret) {
+		klog.Info(fmt.Sprintf("Secret %s already handled", secret.Name))
+		return
+	}
+
+	newSecret, err := MutateSecret(secret)
+	if err != nil {
+		klog.Info(fmt.Sprintf("Error mutating secret %s: %v", secret.Name, err))
+		return
+	}
+
+	patchSecret(clientset, newSecret)
+}
+
+func GeneratePatch(secret v1.Secret) []byte {
+	return []byte(fmt.Sprintf(`[{"op": "add", "path": "/data/%s", "value": "%s"}]`, getRandomSecretKey(secret), base64.StdEncoding.EncodeToString([]byte(getRandomSecretKey(secret)))))
+}
+
+func GenerateRandomSecret(length int, specialChar bool) string {
+	const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	const special = "!@#$%^&*()-_=+[]{}|;:,.<>?/~`"
+
+	rand.Seed(uint64(time.Now().UnixNano()))
+
+	charset := letters
+	if specialChar {
+		charset += special
+	}
+
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = charset[rand.Intn(len(charset))]
+	}
+
+	return string(b)
+}
+
+// patchSecret updates the secret with the new value
+func patchSecret(clientset *kubernetes.Clientset, secret v1.Secret) {
+
+	_, err := clientset.CoreV1().Secrets(secret.Namespace).Update(context.Background(), &secret, metav1.UpdateOptions{})
+	if err != nil {
+		klog.Info(fmt.Sprintf("Error patching secret %s: %v", secret.Name, err))
+	}
+
+	klog.Info(fmt.Sprintf("Secret %s patched", secret.Name))
+
+}
+
+func ReconcileSecrets(clientset *kubernetes.Clientset) error {
+	secretsList, err := clientset.CoreV1().Secrets("").List(context.Background(), metav1.ListOptions{})
+	if err != nil {
+		klog.Infof("Error listing secrets: %v", err)
+		return err
+	}
+	klog.Infof("Found %d secrets\n", len(secretsList.Items))
+	for _, secret := range secretsList.Items {
+		if IsSecretManaged(secret) {
+			HandleSecrets(clientset, secret)
+		}
+	}
+	return nil
+}
+
+func MutateSecret(secret v1.Secret) (v1.Secret, error) {
+	secretCopy := secret.DeepCopy()
+	var length int
+
+	if secretCopy.Annotations[types.OperatorLengthAnnotation] == "" {
+		length = 32
+	} else {
+		var err error
+		length, err = strconv.Atoi(secretCopy.Annotations[types.OperatorLengthAnnotation])
+		if err != nil {
+			klog.Infof("Invalid length annotation, using default length 32: %v", err)
+			length = 32
+		}
+	}
+
+	var specialChar bool
+
+	if secret.Annotations[types.OperatorSpecialCharAnnotation] == "" {
+		specialChar = true
+	} else {
+		var err error
+		specialChar, err = strconv.ParseBool(secret.Annotations[types.OperatorSpecialCharAnnotation])
+		if err != nil {
+			klog.Infof("Invalid specialChar annotation, using default specialChar true: %v", err)
+			specialChar = true
+			return v1.Secret{}, err
+		}
+	}
+
+	randomPass := GenerateRandomSecret(length, specialChar)
+	secretCopy.Data[getRandomSecretKey(secret)] = []byte(randomPass)
+
+	return *secretCopy, nil
+}
